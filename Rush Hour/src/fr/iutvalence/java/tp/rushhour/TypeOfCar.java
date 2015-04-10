e033d503e8414ad4cf4b074792a17e3095717f2d
Charlotte <charlotte.jondet@iut-valence.fr>
2015-04-10 12:37:08 +0200

    Created an enumeration for the different type of cars

---
+++ Rush Hour/src/fr/iutvalence/java/tp/rushhour/TypeOfCar.java
@@ -0,0 +1,17 @@
+package fr.iutvalence.java.tp.rushhour;
+
+/**
+ * 2 type of car 
+ * @author A. Combe, C. Jondet
+ */
+public enum TypeOfCar
+{
+	/**
+	 * Car 
+	 */
+	CAR,
+	/**
+	 * Truck
+	 */
+	TRUCK;
+}
