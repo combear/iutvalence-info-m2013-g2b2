6643b045a0f7b1fb15523429b0b6424973b03e77
Charlotte <charlotte.jondet@iut-valence.fr>
2015-05-05 09:00:14 +0200

    Create exception

---
+++ Rush Hour/src/fr/iutvalence/java/tp/rushhour/OutOfRangeException.java
@@ -0,0 +1,11 @@
+package fr.iutvalence.java.tp.rushhour;
+
+/**
+ * Exception thrown when the position asked is out of the parking range 
+ * @author A. Combe, C. Jondet
+ * 
+ */
+public class OutOfRangeException extends Exception
+{
+
+}
