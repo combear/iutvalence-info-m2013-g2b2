22aabfa0342c6224b8c40d633f8b241ee63cd3d2
Charlotte <charlotte.jondet@iut-valence.fr>
2015-05-05 09:07:18 +0200

    Update each vehicle in the parking, had their head positions

--- Rush Hour/src/fr/iutvalence/java/tp/rushhour/Main.java
+++ Rush Hour/src/fr/iutvalence/java/tp/rushhour/Main.java
@@ -14,14 +14,14 @@ public class Main
 	public static void main(String[] args)
 	{
 		Vehicle[] vehicles = new Vehicle[7];
-		Vehicle vehicleToFree = new Vehicle(VehicleType.CAR, Vehicle.HORIZONTAL);
+		Vehicle vehicleToFree = new Vehicle(VehicleType.CAR, Vehicle.HORIZONTAL,3,3);
 		vehicles[0]= vehicleToFree;
-		vehicles[1]= new Vehicle(VehicleType.TRUCK, Vehicle.VERTICAL);
-		vehicles[2]= new Vehicle(VehicleType.TRUCK, Vehicle.VERTICAL);
-		vehicles[3]= new Vehicle(VehicleType.TRUCK, Vehicle.HORIZONTAL);
-		vehicles[4]= new Vehicle(VehicleType.TRUCK, Vehicle.HORIZONTAL);
-		vehicles[5]= new Vehicle(VehicleType.CAR, Vehicle.VERTICAL);
-		vehicles[6]= new Vehicle(VehicleType.CAR, Vehicle.VERTICAL);
+		vehicles[1]= new Vehicle(VehicleType.TRUCK, Vehicle.VERTICAL,1,1);
+		vehicles[2]= new Vehicle(VehicleType.TRUCK, Vehicle.VERTICAL,1,4);
+		vehicles[3]= new Vehicle(VehicleType.TRUCK, Vehicle.HORIZONTAL,6,3);
+		vehicles[4]= new Vehicle(VehicleType.TRUCK, Vehicle.HORIZONTAL,4,4);
+		vehicles[5]= new Vehicle(VehicleType.CAR, Vehicle.VERTICAL,4,3);
+		vehicles[6]= new Vehicle(VehicleType.CAR, Vehicle.VERTICAL,5,6);
 		
 		RushHourParking parking = new RushHourParking(vehicles, vehicleToFree);
 		RushHourGame game = new RushHourGame(parking);
