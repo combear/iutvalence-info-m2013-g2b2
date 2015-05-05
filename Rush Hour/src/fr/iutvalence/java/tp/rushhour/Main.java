0620f6e8d90e1f3dced6205233d9f520dc154140
Charlotte <charlotte.jondet@iut-valence.fr>
2015-05-05 08:20:51 +0200

    Vehicle array is now initialized in the main and RushHourParking constructor now take this array as a parameter.

--- Rush Hour/src/fr/iutvalence/java/tp/rushhour/Main.java
+++ Rush Hour/src/fr/iutvalence/java/tp/rushhour/Main.java
@@ -7,14 +7,23 @@ package fr.iutvalence.java.tp.rushhour;
  */
 public class Main
 {
-
 	/**
 	 * Application's main: creates a new game and plays it
 	 * @param args command-line arguments (unused)
 	 */
 	public static void main(String[] args)
 	{
-		RushHourParking parking = new RushHourParking(6);
+		Vehicle[] vehicles = new Vehicle[7];
+		Vehicle vehicleToFree = new Vehicle(VehicleType.CAR, Vehicle.HORIZONTAL);
+		vehicles[0]= vehicleToFree;
+		vehicles[1]= new Vehicle(VehicleType.TRUCK, Vehicle.VERTICAL);
+		vehicles[2]= new Vehicle(VehicleType.TRUCK, Vehicle.VERTICAL);
+		vehicles[3]= new Vehicle(VehicleType.TRUCK, Vehicle.HORIZONTAL);
+		vehicles[4]= new Vehicle(VehicleType.TRUCK, Vehicle.HORIZONTAL);
+		vehicles[5]= new Vehicle(VehicleType.CAR, Vehicle.VERTICAL);
+		vehicles[6]= new Vehicle(VehicleType.CAR, Vehicle.VERTICAL);
+		
+		RushHourParking parking = new RushHourParking(vehicles, vehicleToFree);
 		RushHourGame game = new RushHourGame(parking);
 	}
 
--- Rush Hour/src/fr/iutvalence/java/tp/rushhour/RushHourParking.java
+++ Rush Hour/src/fr/iutvalence/java/tp/rushhour/RushHourParking.java
@@ -27,53 +27,17 @@ public class RushHourParking
 	 * Create a parking with a certain number of vehicles and the vehicle to release
 	 * @param numberOfVehicles : total number of vehicles wanted in our parking
 	 */
-	public RushHourParking(int numberOfVehicles)
+	public RushHourParking(Vehicle[] vehicles, Vehicle vehicleToRelease)
 	{
-		this.vehicles= new Vehicle[numberOfVehicles];
-		this.vehicleToRelease=this.addVehicles();
+		this.vehicles = vehicles;
+		this.vehicleToRelease=vehicleToRelease;
 		
 	}
 
-	/**
-	 * Create and place vehicles and the vehicle to release in the parking
-	 * @return the vehicle to free
-	 */
-	private Vehicle addVehicles()
-	{
-		Vehicle vehicleToFree = new Vehicle(TypeOfCar.CAR, Vehicle.HORIZONTAL);
-		this.vehicles[0]= vehicleToFree;
-		this.vehicles[1]= new Vehicle(TypeOfCar.TRUCK, Vehicle.VERTICAL);
-		this.vehicles[2]= new Vehicle(TypeOfCar.TRUCK, Vehicle.VERTICAL);
-		this.vehicles[3]= new Vehicle(TypeOfCar.TRUCK, Vehicle.HORIZONTAL);
-		this.vehicles[4]= new Vehicle(TypeOfCar.TRUCK, Vehicle.HORIZONTAL);
-		this.vehicles[5]= new Vehicle(TypeOfCar.CAR, Vehicle.VERTICAL);
-		this.vehicles[6]= new Vehicle(TypeOfCar.CAR, Vehicle.VERTICAL);
-		return vehicleToFree;
-	}
+	
 
 	//TODO VOIR LE PROBLEME DU NOMBRE DE VEHICLE + LE TABLEAU
 
-
-
-	/**
-	 * Used to get the Array of vehicle of a parking
-	 * @return an array of vehicle
-	 */
-	public Vehicle[] getVehicle() {
-		return this.vehicles;
-	}
-
-
-
-
-	/**
-	 * Used to get the vehicle to free of a parking
-	 * @return the vehicle to free
-	 */
-	public Vehicle getVehicleToFree() {
-		return this.vehicleToRelease;
-	}
-		
 
 	
 }
