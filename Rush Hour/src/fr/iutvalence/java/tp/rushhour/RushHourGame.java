8d30edda656cb47279dae8e0be9d11b4d9fe36da
Charlotte <charlotte.jondet@iut-valence.fr>
2015-05-11 17:34:29 +0200

    Update play() and add  player attribute, isValid() and finished().

--- Rush Hour/src/fr/iutvalence/java/tp/rushhour/RushHourGame.java
+++ Rush Hour/src/fr/iutvalence/java/tp/rushhour/RushHourGame.java
@@ -1,5 +1,7 @@
 package fr.iutvalence.java.tp.rushhour;
 
+import java.util.Scanner;
+
 /**
  * Rush Hour game. The aim of the game is to release a vehicle which is jammed on a parking.
  * http://en.wikipedia.org/wiki/Rush_Hour_%28board_game%29
@@ -13,21 +15,47 @@ public class RushHourGame
 	 */
 	private RushHourParking parking;
 
+	private Player player;
 	/**
 	 * Create a parking which is a set oh vehicle including the one to release
 	 * @param parking : game's parking
 	 */
 	public RushHourGame(RushHourParking parking)
 	{
-		 this.parking = parking;	  
+		 this.parking = parking;
+		 this.player = new Player();
 	}
 	
 	
 	/**launch a RushHour's game*/
 	public void play()
 	{
-		// 
-		
+		Movement movement = null;
+		while (true)
+		{
+			if (finished()) return;
+			
+			while (true)
+			{
+				movement = this.player.getMovement();
+				if (this.isValid(movement)) break;
+			}
+						
+		}
+	}
+
+
+	private boolean isValid(Movement movement)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	private boolean finished()
+	{
+		// TODO Auto-generated method stub
+		return false;
 	}
 	
 }
