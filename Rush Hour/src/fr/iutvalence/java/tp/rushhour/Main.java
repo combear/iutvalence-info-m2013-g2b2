f44eff10aa2baed413bf4179b6d86bcd9749f069
Charlotte <charlotte.jondet@iut-valence.fr>
2015-05-12 17:54:39 +0200

    Changes all the orientation because it was late to do

--- Rush Hour/src/fr/iutvalence/java/tp/rushhour/Main.java
+++ Rush Hour/src/fr/iutvalence/java/tp/rushhour/Main.java
@@ -14,14 +14,14 @@ public class Main
 	public static void main(String[] args)
 	{
 		Vehicle[] vehicles = new Vehicle[7];
-		Vehicle vehicleToFree = new Vehicle(VehicleType.CAR, Vehicle.HORIZONTAL,3,3);
+		Vehicle vehicleToFree = new Vehicle(VehicleType.CAR, Orientation.HORIZONTAL,3,3);
 		vehicles[0]= vehicleToFree;
-		vehicles[1]= new Vehicle(VehicleType.TRUCK, Vehicle.VERTICAL,1,1);
-		vehicles[2]= new Vehicle(VehicleType.TRUCK, Vehicle.VERTICAL,1,4);
-		vehicles[3]= new Vehicle(VehicleType.TRUCK, Vehicle.HORIZONTAL,6,3);
-		vehicles[4]= new Vehicle(VehicleType.TRUCK, Vehicle.HORIZONTAL,4,4);
-		vehicles[5]= new Vehicle(VehicleType.CAR, Vehicle.VERTICAL,4,3);
-		vehicles[6]= new Vehicle(VehicleType.CAR, Vehicle.VERTICAL,5,6);
+		vehicles[1]= new Vehicle(VehicleType.TRUCK, Orientation.VERTICAL,1,1);
+		vehicles[2]= new Vehicle(VehicleType.TRUCK, Orientation.VERTICAL,1,4);
+		vehicles[3]= new Vehicle(VehicleType.TRUCK, Orientation.HORIZONTAL,6,3);
+		vehicles[4]= new Vehicle(VehicleType.TRUCK, Orientation.HORIZONTAL,4,4);
+		vehicles[5]= new Vehicle(VehicleType.CAR, Orientation.VERTICAL,4,3);
+		vehicles[6]= new Vehicle(VehicleType.CAR, Orientation.VERTICAL,5,6);
 		
 		RushHourParking parking = new RushHourParking(vehicles, vehicleToFree);
 		RushHourGame game = new RushHourGame(parking);
