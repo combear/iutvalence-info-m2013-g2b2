7f1d1dca55e33ea06d6ca75f9dc554e340e40aac
Charlotte <charlotte.jondet@iut-valence.fr>
2015-05-12 16:05:05 +0200

    Add length to each vehicleType

--- Rush Hour/src/fr/iutvalence/java/tp/rushhour/VehicleType.java
+++ Rush Hour/src/fr/iutvalence/java/tp/rushhour/VehicleType.java
@@ -1,14 +1,28 @@
 package fr.iutvalence.java.tp.rushhour;
 
 /**
- * 2 type of car 
+ * 2 type of vehicle
  * @author A. Combe, C. Jondet
  */
 public enum VehicleType
 {
 	/**Car*/
-	CAR,
+	CAR(2),
 	
 	/**Truck*/
-	TRUCK;
+	TRUCK(3);
+	
+	private final int length;
+
+	private VehicleType(int length)
+	{
+		this.length = length;
+	}
+	
+	
+	public int getLength()
+	{
+		return this.length;
+	}
+	
 }
