b45ff3c4e8e92ddefad11b6e00bd5826f2762695
Charlotte <charlotte.jondet@iut-valence.fr>
2015-05-12 14:40:58 +0200

    Add javadoc

--- Rush Hour/src/fr/iutvalence/java/tp/rushhour/Player.java
+++ Rush Hour/src/fr/iutvalence/java/tp/rushhour/Player.java
@@ -1,7 +1,16 @@
 package fr.iutvalence.java.tp.rushhour;
 
+/**
+ * Player class, simulation of user's choices
+ * @author A. Combe, C. Jondet
+ *
+ */
 public class Player
 {
+	/**
+	 * Allows to send a new position 
+	 * @return movement
+	 */
 	public Movement getMovement()
 	{
 		return new Movement(new Position(1,1), new Position(4,1));			
