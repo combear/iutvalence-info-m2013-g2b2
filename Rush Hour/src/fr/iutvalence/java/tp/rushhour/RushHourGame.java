dc5660c07969194812e48fd8dd4c578469174329
Charlotte <charlotte.jondet@iut-valence.fr>
2015-05-13 17:04:47 +0200

    Create processMovement, moveVehicle and forward. processMovement: allows to release movement moveVehicle: waiting forward: forward vehicle

--- Rush Hour/src/fr/iutvalence/java/tp/rushhour/RushHourGame.java
+++ Rush Hour/src/fr/iutvalence/java/tp/rushhour/RushHourGame.java
@@ -1,11 +1,9 @@
 package fr.iutvalence.java.tp.rushhour;
 
-import java.awt.Dialog.ModalExclusionType;
-import java.util.Scanner;
-
 /**
- * Rush Hour game. The aim of the game is to release a vehicle which is jammed on a parking.
- * http://en.wikipedia.org/wiki/Rush_Hour_%28board_game%29
+ * Rush Hour game. The aim of the game is to release a vehicle which is jammed
+ * on a parking. http://en.wikipedia.org/wiki/Rush_Hour_%28board_game%29
+ * 
  * @author A. Combe, C. Jondet
  *
  */
@@ -17,215 +15,89 @@ public class RushHourGame
 	private RushHourParking parking;
 
 	private Player player;
+
 	/**
 	 * Create a parking which is a set oh vehicle including the one to release
-	 * @param parking : game's parking
+	 * 
+	 * @param parking
+	 *            : game's parking
 	 */
 	public RushHourGame(RushHourParking parking)
 	{
-		 this.parking = parking;
-		 this.player = new Player();
+		this.parking = parking;
+		this.player = new Player();
 	}
-	
-	
-	/**launch a RushHour's game*/
+
+	/** launch a RushHour's game */
 	public void play()
 	{
 		Movement movement = null;
 		while (true)
 		{
-			if (finished()) return;
-			
+			if (finished())
+				return;
+
 			while (true)
 			{
 				movement = this.player.getMovement();
-				if (this.isValid(movement)) break;
+				if (this.isValid(movement))
+					break;
 			}
-						
+
 		}
 	}
-
 
 	/**
-	 * Say if a movement is feasible
-	 * @param movement : the movement we ask if it's possible
-	 * @return true if it's possible false if it's not
+	 * Prepare the movement if it's possible to accomplish
+	 * @param movement
+	 * @return
 	 */
-	private boolean isValid(Movement movement)
+	public boolean processMovement(Movement movement)
 	{
-		try {
-			
-			if (parking.isEmpty(movement.getStart()))
-				return false;
-			}
-			catch (OutOfRangeException e)
-			{
-				e.printStackTrace();
-			}
-		if (movement.getStart().isAligned(movement.getEnding(), parking.getVehicleOn(movement.getStart()).getOrientation())) return false;
-		
 		Vehicle vehicleToMove = parking.getVehicleOn(movement.getStart());
+		if (vehicleToMove == null) return false;
 		
+		if (!(movement.getOrientation() == vehicleToMove.getOrientation())) return false;
 		
-		if (vehicleToMove.getVehicleType() == VehicleType.CAR)
-			{
-				if (vehicleToMove.getOrientation() == Orientation.HORIZONTAL) 
-				
-					{
-						if (movement.getStart().getX() > movement.getEnding().getX())
-						
-						{
-							for(int movementIndex=(movement.getStart().getX())-1;movementIndex<movement.getEnding().getX();movementIndex--)
-							{
-								try {
-									if (parking.isEmpty(new Position(movementIndex,movement.getStart().getY()))) return false;
-								}
-								catch (OutOfRangeException e)
-								{
-									return false;
-								}
-								
-							}
-							return true;
-						}
-						else if (movement.getStart().getX() < movement.getEnding().getX())
-							
-						{
-							for(int movementIndex=(movement.getStart().getX());movementIndex>movement.getEnding().getX();movementIndex++)
-							{
-								try {
-									if (parking.isEmpty(new Position(movementIndex,movement.getStart().getY()))) return false;
-								}
-								catch (OutOfRangeException e)
-								{
-									return false;
-								}
-								
-							}
-							return true;
-						}
-					}
-				else if (vehicleToMove.getOrientation() == Orientation.VERTICAL)
-				{
-					if (movement.getStart().getY() > movement.getEnding().getY())
-					
-					{
-						for(int movementIndex=(movement.getStart().getY())-1;movementIndex<movement.getEnding().getY();movementIndex--)
-						{
-							try {
-								if (parking.isEmpty(new Position(movementIndex,movement.getStart().getX()))) return false;
-							}
-							catch (OutOfRangeException e)
-							{
-								return false;
-							}
-							
-						}
-						return true;
-					}
-					else if (movement.getStart().getY() < movement.getEnding().getY())
-						
-					{
-						for(int movementIndex=(movement.getStart().getY());movementIndex>movement.getEnding().getY();movementIndex++)
-						{
-							try {
-								if (parking.isEmpty(new Position(movementIndex,movement.getStart().getX()))) return false;
-							}
-							catch (OutOfRangeException e)
-							{
-								return false;
-							}
-							
-						}
-						return true;
-					}
-				}
-			}
-		else if (vehicleToMove.getVehicleType() == VehicleType.TRUCK)
-		{
-			if (vehicleToMove.getOrientation() == Orientation.HORIZONTAL) 
-			
-				{
-					if (movement.getStart().getX() > movement.getEnding().getX())
-					
-					{
-						for(int movementIndex=(movement.getStart().getX())-2;movementIndex<movement.getEnding().getX();movementIndex--)
-						{
-							try {
-								if (parking.isEmpty(new Position(movementIndex,movement.getStart().getY()))) return false;
-							}
-							catch (OutOfRangeException e)
-							{
-								return false;
-							}
-							
-						}
-						return true;
-					}
-					else if (movement.getStart().getX() < movement.getEnding().getX())
-						
-					{
-						for(int movementIndex=(movement.getStart().getX());movementIndex>movement.getEnding().getX();movementIndex++)
-						{
-							try {
-								if (parking.isEmpty(new Position(movementIndex,movement.getStart().getY()))) return false;
-							}
-							catch (OutOfRangeException e)
-							{
-								return false;
-							}
-							
-						}
-						return true;
-					}
-				}
-			else if (vehicleToMove.getOrientation() == Orientation.VERTICAL)
-			{
-				if (movement.getStart().getY() > movement.getEnding().getY())
-				
-				{
-					for(int movementIndex=(movement.getStart().getY())-2;movementIndex<movement.getEnding().getY();movementIndex--)
-					{
-						try {
-							if (parking.isEmpty(new Position(movementIndex,movement.getStart().getX()))) return false;
-						}
-						catch (OutOfRangeException e)
-						{
-							return false;
-						}
-						
-					}
-					return true;
-				}
-				else if (movement.getStart().getY() < movement.getEnding().getY())
-					
-				{
-					for(int movementIndex=(movement.getStart().getY());movementIndex>movement.getEnding().getY();movementIndex++)
-					{
-						try {
-							if (parking.isEmpty(new Position(movementIndex,movement.getStart().getX()))) return false;
-						}
-						catch (OutOfRangeException e)
-						{
-							return false;
-						}
-						
-					}
-					return true;
-				}
-			}
-		}
-		return false;
+		int delta = movement.getDelta(vehicleToMove.getOrientation());
 		
-					
-			
+		return this.moveVehicle(vehicleToMove, delta);	
+		
 	}
 
-
-	private boolean finished()
+	/**
+	 * Move the vehicle
+	 * @param vehicleToMove
+	 * @param delta
+	 * @return
+	 */
+	private boolean moveVehicle(Vehicle vehicleToMove, int delta)
 	{
-		// TODO Auto-generated method stub
+		
+		if (vehicleToMove.getOrientation() == Orientation.HORIZONTAL)
+			parking.isEmpty(new Position(movement.getStart().getX()-delta));
+		
+		moveVehicle(vehicleToMove, delta+1);
+			
 		return false;
+	}
+
+	/**
+	 * Used to move the vehicle forward
+	 * @param vehicleToMove 
+	 * @return if moved or not
+	 */
+	private boolean forward(Vehicle vehicleToMove)
+	{
+		if (vehicleToMove.getOrientation()== Orientation.HORIZONTAL)
+			parking.isEmpty(position)
 	}
 	
+	private boolean finished()
+	{
+		if (parking.getVehicleToRelease().isOn(new Position(6, 3)))
+			return true;
+		return false;
+	}
+
 }
