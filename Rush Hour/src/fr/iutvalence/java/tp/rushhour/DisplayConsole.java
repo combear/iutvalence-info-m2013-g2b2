1f505ec84c4c1b7c83dee9427db99a2b7405bf48
Charlotte <charlotte.jondet@iut-valence.fr>
2015-05-22 15:31:24 +0200

    Changes

--- Rush Hour/src/fr/iutvalence/java/tp/rushhour/DisplayConsole.java
+++ Rush Hour/src/fr/iutvalence/java/tp/rushhour/DisplayConsole.java
@@ -4,24 +4,31 @@ package fr.iutvalence.java.tp.rushhour;
  * @author A. Combe, C. Jondet
  *
  */
-public class DisplayConsole
-{
+public class DisplayConsole {
 
-	public String createGrid()
-	{
-		String grid="";
-		grid+="1  2  3  4  5  6\n";
-		
-		for(int compteurEtoile=0; compteurEtoile<26; compteurEtoile++)
-		{
-			grid+="*";
-		}
-		grid+="\n";
-		return grid;
-	}
-	
-	
-	
-	
-	
+
+public String createGrid() {
+        String grid = "";
+        grid += "   1    2    3    4    5    6 \n  ";
+
+        for (int compteurEtoile = 0; compteurEtoile < 29; compteurEtoile++) {
+                grid += "*";
+        }
+                grid += "\n";
+        for (int compteurVertical =1; compteurVertical <7; compteurVertical++){
+        	
+        	grid+= compteurVertical;
+                for (int compteurHorizontal=1; compteurHorizontal<7;compteurHorizontal++)
+                {
+                	grid += " |  |";                	
+                }
+                grid+= "\n";
+        
+              
+        }
+        grid += "\n";
+        
+        return grid;
+}
+
 }
